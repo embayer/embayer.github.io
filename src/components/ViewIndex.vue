6a6518102bf58f4806192b4f923b247bf99b3f8c
Markus Bayer <bayer_markus@web.de>
2018-03-31 13:19:12 +0200

    implemented index view

---
+++ src/components/ViewIndex.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="index">
+    <div>
+      <span>Date</span>
+      <span>{{now}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  name: 'ViewIndex',
+  data () {
+    return {
+      now: moment().format('DD.MM.YYYY HH:MM')
+    }
+  }
+}
+</script>
+
+<style>
+</style>

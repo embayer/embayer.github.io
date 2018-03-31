f5649d709ec4b3aeddcb044ef95c6f8dbf25fbe5
Markus Bayer <bayer_markus@web.de>
2018-03-31 19:02:48 +0200

    using getTimedeltaObject

--- src/components/ViewIndex.vue
+++ src/components/ViewIndex.vue
@@ -1,21 +1,64 @@
 <template>
   <div id="index">
-    <div>
-      <span>Date</span>
-      <span>{{now}}</span>
-    </div>
+
+      <table>
+        <thead>
+        <tr>
+          <th>Key</th>
+          <th>Property</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>Now</td>
+          <td>{{now}}</td>
+        </tr>
+        <tr>
+          <td>pack amount</td>
+          <td>{{packAmount}}</td>
+        </tr>
+        <tr>
+          <td>pack price</td>
+          <td>{{packPrice}}</td>
+        </tr>
+        <tr>
+          <td>Delta Object</td>
+          <td>{{timedelta}}</td>
+        </tr>
+        </tbody>
+      </table>
+
   </div>
 </template>
 
 <script>
 import moment from 'moment'
+import {
+  PACK_AMOUNT,
+  PACK_PRICE
+} from '../constants'
+import {
+  getTimedeltaObject
+} from './functions'
 
 export default {
   name: 'ViewIndex',
   data () {
     return {
-      now: moment().format('DD.MM.YYYY HH:MM')
+      packAmount: PACK_AMOUNT,
+      packPrice: PACK_PRICE,
+      lastCigarette: moment('01-01-2016', 'MM-DD-YYYY')
     }
+  },
+  computed: {
+    timedelta: function () {
+      return getTimedeltaObject(this.lastCigarette, this.now)
+    },
+    now: function () {
+      return moment()
+    }
+  },
+  methods: {
   }
 }
 </script>

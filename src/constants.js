5f57e3ec228e654bf5b1029f907f1ea4d82649cb
Markus Bayer <bayer_markus@web.de>
2018-04-01 16:56:09 +0200

    installed i18n

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,17 @@
 export const PACK_AMOUNT = 20
 export const PACK_PRICE = 6.20
 export const CIGARETTES_PER_DAY = 15
+
+// Ready translated locale messages
+export const strings = {
+  en: {
+    message: {
+      hello: 'hello world'
+    }
+  },
+  de: {
+    message: {
+      hello: 'grieß di'
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,13 +3,24 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueI18n from 'vue-i18n'
+import {strings} from './constants'
+
+Vue.use(VueI18n)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: 'de', // set locale
+  strings // set locale messages
+})
+
+// Create a Vue instance with `i18n` option
 new Vue({
+  i18n,
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
-})
+}).$mount('#app')

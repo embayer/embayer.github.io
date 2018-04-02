27735aa501247d4483ff115318acb826afbc666a
Markus Bayer <bayer_markus@web.de>
2018-04-02 17:45:55 +0200

    added date pluralization

--- src/constants.js
+++ src/constants.js
@@ -5,12 +5,22 @@ export const CIGARETTES_PER_DAY = 15
 // Ready translated locale messages
 export const messages = {
   en: {
+    year: 'year | years',
+    month: 'month | months',
+    day: 'day | days',
+    hour: 'hour | hours',
+    minute: 'minute | minutes',
     msg: {
       timedelta: `{y} years, {M} months, {d} days, {h} hours, {m} minutes.`,
       hello: 'hello world'
     }
   },
   de: {
+    year: 'Jahr | Jahre',
+    month: 'Monat | Monate',
+    day: 'Tag | Tage',
+    hour: 'Stunde | Stunden',
+    minute: 'Minute | Minuten',
     msg: {
       timedelta: `{y} years, {M} months, {d} days, {h} hours, {m} minutes.`,
       hello: 'grieß di'
